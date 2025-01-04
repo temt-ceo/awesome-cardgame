a3ef0f642814f859a2893ece2fb0bcee9dbb2388
田原崇 <taharatakashi@taharatakashinoMacBook-Air-2.local>
2025-01-05 08:13:56 +0900

    Fine Tuning

--- sendFlow.js
+++ sendFlow.js
@@ -1,6 +1,5 @@
 /* 注意: ES moduleでは動かないので、フォルダから取り出して実行して下さい。 "type": "module"の影響の及ばないところで実行できます。 */
 const fcl = require("@onflow/fcl");
-const t = require("@onflow/types");
 const { SHA3 } = require("sha3");
 var EC = require("elliptic").ec;
 var ec = new EC("p256");
